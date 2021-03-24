c8082d15759b6a9e2fbc2eaa4e5a799300253a13
Rodolfo Della Violla <rodolfo.violla@eduzz.com>
2021-03-24 19:42:04 -0300

    Implements jwt token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,47 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password is invalid');
+    }
+
+    const isPasswordCorrect = await compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new Error('Email or password is invalid');
+    }
+
+    const { id, name } = user;
+
+    const token = sign({}, process.env.APP_SECRET, { subject: id, expiresIn: '1d' });
+
+    return { user: { id, email, name }, token };
+  }
+}
